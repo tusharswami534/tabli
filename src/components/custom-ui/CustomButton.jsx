a74954b264c13a1194955e874e62591f2c8239b4
TUSHAR SWAMI <tusharswami17@gamil.com>
2025-04-22 11:20:52 +0530

    add lh

---
+++ src/components/custom-ui/CustomButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CustomButton = ({buttonName}) => {
+  return (
+    <button className={`px-4 pt-3.5 shadow-common-button transition-all duration-300 bg-yellow rounded-[40px] text-dark-blue pb-2.5`}>
+      {buttonName}
+    </button>
+  )
+}
+
+export default CustomButton
+
