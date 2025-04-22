71c718105a6cc5c3ddae6da9373f89ce5e7a3d02
Ankush Dhull <apple@Apples-MacBook-Air.local>
2025-04-22 14:19:34 +0530

    vantaggi sec done

---
+++ src/utils/helper.js
@@ -0,0 +1,39 @@
+export const BENEFITS_LIST = [
+    {
+        image:"/assets/images/webp/benefits-one.webp",
+        title:"Massima libertà",
+        description:"Evita code o tempi morti: fai tutto in autonomia, direttamente dal tuo smartphone.",
+        descriptionTwo:"Con Tabli, i tuoi clienti avranno un’esperienza più fluida e piacevole, mentre tu potrai concentrarti su ciò che conta davvero: offrire un servizio di qualità."
+    },
+    {
+        image:"/assets/images/webp/benefits-three.webp",
+        title:"Zero attese inutili",
+        description:"Evita code o tempi morti: fai tutto in autonomia, direttamente dal tuo smartphone.",
+        descriptionTwo:"Con Tabli, i tuoi clienti avranno un’esperienza più fluida e piacevole, mentre tu potrai concentrarti su ciò che conta davvero: offrire un servizio di qualità."
+    },
+    {
+        image:"/assets/images/webp/benefits-two.webp",
+        title:"Esperienza su misura",
+        description:"Evita code o tempi morti: fai tutto in autonomia, direttamente dal tuo smartphone.",
+        descriptionTwo:"Con Tabli, i tuoi clienti avranno un’esperienza più fluida e piacevole, mentre tu potrai concentrarti su ciò che conta davvero: offrire un servizio di qualità."
+    },
+    {
+        image:"/assets/images/webp/benefits-one.webp",
+        title:"Ordini chiari e precisi",
+        description:"Evita code o tempi morti: fai tutto in autonomia, direttamente dal tuo smartphone.",
+        descriptionTwo:"Con Tabli, i tuoi clienti avranno un’esperienza più fluida e piacevole, mentre tu potrai concentrarti su ciò che conta davvero: offrire un servizio di qualità."
+    },
+    {
+        image:"/assets/images/webp/benefits-three.webp",
+        title:"Sempre con te",
+        description:"Evita code o tempi morti: fai tutto in autonomia, direttamente dal tuo smartphone.",
+        descriptionTwo:"Con Tabli, i tuoi clienti avranno un’esperienza più fluida e piacevole, mentre tu potrai concentrarti su ciò che conta davvero: offrire un servizio di qualità."
+    },
+    {
+        image:"/assets/images/webp/benefits-two.webp",
+        title:"Assistenza Immediata",
+        description:"Evita code o tempi morti: fai tutto in autonomia, direttamente dal tuo smartphone.",
+        descriptionTwo:"Con Tabli, i tuoi clienti avranno un’esperienza più fluida e piacevole, mentre tu potrai concentrarti su ciò che conta davvero: offrire un servizio di qualità."
+    },
+   
+]
