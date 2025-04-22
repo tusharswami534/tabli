5def1e709cd9c5a887e84d0cc3ff744cab13c612
Bimla-jain-555 <vj13798@gmail.com>
2025-04-22 12:47:39 +0530

    change position of menu on 2xl screen

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,10 +4,10 @@ import Image from 'next/image'
 const Footer = () => {
   return (
     <div className='relative '>
-      <div className='absolute 2xl:top-[-82%] xl:top-[-50%] md:top-[-30%] top-[-10%] left-0 w-full h-full'>
-        <Image src="/assets/images/png/footer-top-layer.png" alt="Footer Image" width={1920} height={1080} className="w-full h-auto"/>
+      <div className='absolute 2xl:top-[-82%] xl:top-[-50%] md:top-[-30%] top-[-10%] left-0 w-full h-full '>
+        <Image src="/assets/images/png/footer-top-layer.png" alt="Footer Image" width={1920} height={1080} className="w-full 2xl:h-[200px]"/>
       </div>
-      <div className='absolute 2xl:top-[-168%] xl:top-[-140%] lg:top-[-110%] md:top-[-60%] top-[-50%] sm:left-[9%] left-[14%]'>
+      <div className='absolute 2xl:top-[-168%] xl:top-[-140%] lg:top-[-110%] md:top-[-60%] top-[-50%] sm:left-[9%] left-[14%] 2xl:left-[35%]'>
         <Image src="/assets/images/png/menu.png" alt='menu' width={729} height={517} className='2xl:w-[729px] xl:w-[600px] lg:w-[500px] md:w-[300px] w-[236px]'/>
       </div>
       <div className='bg-gray'>
