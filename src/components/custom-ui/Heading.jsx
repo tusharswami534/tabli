67d4eb2a1ddc7c9a4bdca99b22bd39feaf3b04b4
Ankush Dhull <apple@Apples-MacBook-Air.local>
2025-04-22 15:02:01 +0530

    match spacing

--- src/components/custom-ui/Heading.jsx
+++ src/components/custom-ui/Heading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Heading = ({ headingText, className }) => {
   return (
-    <h2 className={`text-dark-blue font-black text-40 leading-100 italic ${className} `}>{headingText}</h2>
+    <h2 className={`md:text-dark-blue text-dark-grey font-black md:text-40 text-32  leading-100 italic ${className} `}>{headingText}</h2>
   )
 }
 
--- src/components/home/VantaggiPerTe.jsx
+++ src/components/home/VantaggiPerTe.jsx
@@ -13,13 +13,13 @@ const VantaggiPerTe = () => {
     <div>
       <Image className='w-full object-cover' height={217} width={1920} src={'/assets/images/png/benefits-top-layer.png'} alt='top layer' />
       <div className="bg-yellow translate-y-[-1px] pb-[180px]">
-        <div className="py-10">
+        <div className="md:py-10 py-6">
           <Heading className='text-center' headingText='Vantaggi per Te' />
         </div>
-        <div className="max-w-[1580px] mx-auto flex items-center justify-center">
+        <div className="max-w-[1560px] items-center justify-center overflow-x-auto scrollbar-hide mx-auto flex">
           <div className="flex items-center justify-center max-2xl:hidden gap-x-3 py-[89px] h-[588px] ">
             {BENEFITS_LIST.map((item, index) => (
-              <div key={index} className="bg-white group rounded-3xl transition-all duration-300 overflow-hidden hover:max-w-[300px] max-w-[240px] w-full">
+              <div key={index} className="bg-white group rounded-3xl transition-all duration-300 overflow-hidden  hover:w-[300px] w-[240px]">
                 <Image className='w-[240px] h-[300px] group-hover:w-[300px] transition-all duration-300' height={300} width={240} src={item.image} alt='benefits' />
                 <div className="flex flex-col py-6 px-4 gap-2">
                   <p className='text-dark-blue font-bold leading-100 text-xl text-center'>{item.title}</p>
@@ -31,35 +31,41 @@ const VantaggiPerTe = () => {
               </div>
             ))}
           </div>
-          <div className="2xl:hidden overflow-hidden">
-            <Swiper
-              spaceBetween={12}
-              modules={[Pagination]}
-              pagination={{ clickable: true }}
-              centeredSlides={true}
-              centeredSlidesBounds={true}
-              loop={true}
-              slidesOffsetBefore={20}
-              slidesOffsetAfter={20}
-              breakpoints={{
-                1024: { slidesPerView: 4 },
-                768: { slidesPerView: 2.2, spaceBetween: 30 },
-                640: { slidesPerView: 1.6 },
-                320: { slidesPerView: 1.2 },
-              }}
-              className="flex items-center justify-center max-w-[1240px] mx-auto py-[89px] pb-[40px]"
-            >
-              {BENEFITS_LIST.map((item, index) => (
-                <SwiperSlide key={index} className="bg-white group rounded-3xl transition-all duration-300 overflow-hidden max-w-[300px] w-full">
-                  <Image className='w-[300px] max-md:h-[300px]' height={300} width={240} src={item.image} alt='benefits' />
-                  <div className="flex flex-col py-6 px-4 gap-2">
-                    <p className='text-dark-blue font-bold leading-100 text-xl'>{item.title}</p>
-                    <Paragraph className='!text-gray !leading-[130%]' paragraphText={item.description} />
-                  </div>
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
+        </div>
+        <div className="2xl:hidden overflow-hidden">
+          <Swiper
+            spaceBetween={12}
+            modules={[Pagination]}
+            pagination={{ clickable: true }}
+            slidesPerGroup={1}
+            breakpoints={{
+              1024: {
+                slidesPerView: 4, centeredSlides: false,
+                centeredSlidesBounds: false
+              },
+              768: {
+                slidesPerView: 2.2, spaceBetween: 30,
+                centeredSlides: true,
+                centeredSlidesBounds: true
+              },
+              640: { slidesPerView: 1.6 },
+              320: {
+                slidesPerView: 1.2,
+                centeredSlides: true,
+                centeredSlidesBounds: true
+              },
+            }}
+            className="flex items-center justify-center max-w-[1320px] mx-auto py-[89px] pb-[40px]">
+            {BENEFITS_LIST.map((item, index) => (
+              <SwiperSlide key={index} className="bg-white group rounded-3xl transition-all duration-300 overflow-hidden max-w-[300px] w-full">
+                <Image className='w-[300px] max-md:h-[300px]' height={300} width={240} src={item.image} alt='benefits' />
+                <div className="flex flex-col py-6 px-4 gap-2">
+                  <p className='text-dark-blue font-bold leading-100 text-xl'>{item.title}</p>
+                  <Paragraph className='!text-gray !leading-[130%]' paragraphText={item.description} />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
     </div >
