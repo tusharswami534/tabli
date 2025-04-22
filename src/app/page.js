fa6db445abbc669b733f77f24d7b7f34a6626662
Bimla-jain-555 <vj13798@gmail.com>
2025-04-22 11:37:44 +0530

    add footer section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Footer from "@/components/common/Footer";
+import Contact from "@/components/valorizza/Contact";
 import Image from "next/image";
 
 export default function Home() {
   return (
-  <>
+    <>
+      <Contact/>
     <Footer/>
   </>
   );
--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -3,12 +3,19 @@ import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div className='relative bg-'>
-      <div className='absolute top-0 left-0 w-full h-full'>
+    <div className='relative '>
+      <div className='absolute 2xl:top-[-82%] xl:top-[-50%] md:top-[-30%] top-[-10%] left-0 w-full h-full'>
         <Image src="/assets/images/png/footer-top-layer.png" alt="Footer Image" width={1920} height={1080} className="w-full h-auto"/>
       </div>
-      <div className="container mx-auto px-4 ">
+      <div className='absolute 2xl:top-[-168%] xl:top-[-140%] lg:top-[-110%] md:top-[-60%] top-[-50%] sm:left-[9%] left-[14%]'>
+        <Image src="/assets/images/png/menu.png" alt='menu' width={729} height={517} className='2xl:w-[729px] xl:w-[600px] lg:w-[500px] md:w-[300px] w-[236px]'/>
+      </div>
+      <div className='bg-gray'>
+      <div className="container mx-auto px-4 pt-20 pb-10">
+        <Image src="/assets/images/png/footer-logo.png" alt='footer-logo' width={123} height={33} className='w-[123px] pb-6 flex mx-auto' />
+        <p className='text-base italic font-[900] text-white text-center'>Tutti i diritti riservati</p>
 
+        </div>
       </div>
     </div>
   )
