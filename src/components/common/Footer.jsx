8e4230bd5a6b51edec60b8d7fd9886b93f3533f4
Ankush Dhull <apple@Apples-MacBook-Air.local>
2025-04-23 16:32:41 +0530

    change img

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -3,14 +3,14 @@ import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div className='relative bg-gray max-sm:pt-14'>
-      <div className='absolute 2xl:top-[-82%] xl:top-[-50%] md:top-[-30%] top-[-10%] left-0 w-full h-full '>
-        <Image src="/assets/images/png/footer-top-layer.png" alt="Footer Image" width={1920} height={1080} className="w-full 2xl:h-[200px]"/>
+    <div className='relative bg-gray'>
+      <div className='absolute 2xl:top-[-82%] xl:top-[-50%] md:top-[-30%] top-[-25%] left-0 w-full h-full pointer-events-none'>
+        <Image src="/assets/images/png/footer-top-layer.png" alt="Footer Image" width={1920} height={1080} className="2xl:h-[200px] w-[1920px] pointer-events-none object-cover"/>
       </div>
       <div className='max-w-[1920px] mx-auto '> 
         <div className='bg-gray'>
-          <div className="container mx-auto px-4 pt-20 pb-10">
-            <Image src="/assets/images/png/footer-logo.png" alt='footer-logo' width={123} height={33} className='w-[123px]  pb-6 flex mx-auto' />
+          <div className="container mx-auto px-4 py-7 sm:pt-20 sm:pb-10">
+            <Image src="/assets/images/png/footer-logo.png" alt='footer-logo' width={123} height={33} className='w-[123px] pb-2 sm:pb-6 flex mx-auto' />
             <p className='text-base italic font-[900] text-white text-center'>Tutti i diritti riservati</p>
           </div>
         </div>
