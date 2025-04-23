a1fa0b80f7585b92300511a7f21097bbad41ffc1
sonambishnoi550 <bishnoisonam079@gmail.com>
2025-04-23 15:44:32 +0530

    updates done

--- src/components/valorizza/SoluzioniMigliorare.jsx
+++ src/components/valorizza/SoluzioniMigliorare.jsx
@@ -6,19 +6,19 @@ const SoluzioniMigliorare = () => {
   return (
     <div>
       <div className="bg-dark-blue relative -mt-1 md:pt-10">
-        <Image className="absolute lg:-top-[13%] md:-top-[8%] -top-[5%] md:h-[136px] h-[94px] w-full" src="/assets/images/png/Soluzioni-top-layer.png" alt="top-layer" width={1920} height={136} />
-        <Image className="absolute 2xl:-bottom-[12%] lg:-bottom-[16%] md:-bottom-[10%] md:h-[136px] h-[94px] max-[765px]:-bottom-[8%] max-[450px]:-bottom-[6%] w-full" src="/assets/images/png/Soluzioni-bottom-layer.png" alt="bottom-layer" width={1920} height={152} />
+        <Image className="absolute xl:-top-[13%] lg:top-[-17%] md:-top-[11%] sm:-top-[9%] top-[-5%] md:h-[136px] sm:h-[94px] h-[67px]  w-full" src="/assets/images/png/Soluzioni-top-layer.png" alt="top-layer" width={1920} height={136} />
+        <Image className="absolute 2xl:-bottom-[12%] lg:-bottom-[15%] md:-bottom-[10%] md:h-[136px] sm:h-[94px] h-[67px] max-[765px]:-bottom-[8%] max-[640px]:-bottom-[5%] max-[500px]:-bottom-[4%] w-full" src="/assets/images/png/Soluzioni-bottom-layer.png" alt="bottom-layer" width={1920} height={152} />
         <Image className="absolute h-auto left-0 w-[260px] xl:block hidden top-[5%]" src="/assets/images/png/soluzioni-left-layer.png" alt="top-layer" width={1920} height={136} />
-        <Image className="absolute h-auto right-0 2xl:max-w-[379px] max-w-[250px] xl:block hidden" src="/assets/images/png/soluzioni-right-layer.png" alt="bottom-layer" width={1920} height={152} />
-        <div className="max-w-[1560px] mx-auto px-4">
+        <Image className="absolute h-auto right-0 2xl:top-[10%] 2xl:max-w-[379px] max-w-[250px] xl:block hidden" src="/assets/images/png/soluzioni-right-layer.png" alt="bottom-layer" width={1920} height={152} />
+        <div className="max-w-[1560px] mx-auto px-6">
           <div className="flex max-lg:flex-col lg:gap-20 gap-10 max-xl:justify-center pt-10 lg:pb-[100px] mb:pb-20 relative z-10 max-w-[1408px] mx-auto">
             <Image className="xl:max-w-[656px] xl:h-[640px] lg:max-w-[427px] md:max-w-[467px] max-w-[350px] w-full md:h-[500px] max-lg:mx-auto" src="/assets/images/webp/soluzioni.webp" alt="soluzioni-image" width={656} height={640} />
             <div>
-              <Heading headingText="  Soluzioni per migliorare il tuo lavoro" className="!text-center lg:!max-w-[465px] max-lg:!text-center max-sm:!text-left !max-w-none max-lg:!text-40 !leading-130 !text-yellow xl:!pb-10 !pb-6" />
+              <Heading headingText="Soluzioni per migliorare il tuo lavoro" className="lg:!max-w-[465px] max-lg:!text-center max-sm:!text-left !max-w-none max-lg:!text-40 !leading-130 !text-yellow xl:!pb-10 !pb-6" />
 
               <div>
                 {CONTENT_LIST.map((item, index) => (
-                  <div key={index} className="xl:pb-8 pb-5">
+                  <div key={index} className="pb-8">
                     <h4 className="text-26 font-bold text-white italic max-lg:text-center max-sm:text-left max-sm:pb-3 !leading-130">
                       {item.title}
                     </h4>
--- src/components/valorizza/VantaggiAttività.jsx
+++ src/components/valorizza/VantaggiAttività.jsx
@@ -6,23 +6,23 @@ const VantaggiAttività = () => {
 
     return (
         <div className='bg-dark-blue relative pt-10 '>
-            <Image src="/assets/images/webp/outline.webp" alt='outline' width={1920} height={1080} className='w-full absolute 2xl:bottom-[-31%] min-[1200px]:-bottom-[21%] z-20  h-auto xl:block hidden' />
+            <Image src="/assets/images/webp/outline.webp" alt='outline' width={1920} height={1080} className='w-full absolute 2xl:bottom-[-16%] min-[1200px]:-bottom-[21%] z-20  h-auto xl:block hidden min-[2000px]:!bottom-[-45%]' />
             <Image src="/assets/images/webp/tabli-name.webp" alt='tabli name' width={585} height={158} className=' absolute min-[2000px]:!left-[45%] lg:top-[-23%] md:top-[-19%] top-[-10%] xl:left-[30%] lg:left-[18%] md:left-[10%] left-[9%] max-w-[585px] mx-auto w-full max-md:w-[292px]' />
             <Image className='absolute lg:top-[-16%] sm:-top-[13%] -top-[7%] sm:h-[152px] h-[88px] w-full' src="/assets/images/png/attivita-top-layer.png" alt='top-layer' width={1920} height={152} />
             <Image className='absolute lg:-bottom-[15%] z-10 md:-bottom-[12%] -bottom-[8%] w-full md:h-[152px] h-[88px]' src="/assets/images/webp/attivita-bottom-layer.webp" alt='bottom-layer' width={1920} height={152} />
-            <Image src="/assets/images/webp/tabli-yellow .webp" alt='tabli name' width={585} height={158} className=' absolute lg:bottom-[-8%] md:bottom-[-9%] bottom-[-5%] xl:left-[30%] lg:left-[18%] md:left-[10%] left-[9%] max-w-[585px] mx-auto w-full z-20 max-sm:w-[292px] min-[2000px]:!left-[45%]' />
+            <Image src="/assets/images/webp/tabli-yellow .webp" alt='tabli name' width={585} height={158} className=' absolute lg:bottom-[-8%] md:bottom-[-9%] bottom-[-5%] xl:left-[30%] lg:left-[18%] md:left-[10%] left-[9%] max-w-[585px] mx-auto w-full max-sm:top-[100%] z-20 max-sm:w-[292px] min-[2000px]:!left-[45%]' />
 
-            <div className="max-w-[1408px] mx-auto px-4">
+            <div className="max-w-[1408px] mx-auto px-6">
                 <Heading headingText="Vantggi per le tau Attivita" className="!text-center max-lg:!text-40 !leading-normal !text-yellow" />
-                <div className="flex max-lg:flex-col lg:justify-between relative z-30 max-lg:justify-center md:pt-10 md:pb-[140px] pb-20 max-w-[1408px] mx-auto">
-                    <Image className='xl:max-w-[750px] max-w-[540px] max-lg:mx-auto w-full' src="/assets/images/webp/attivita-image.webp" alt="attivita" width={800.6} height={534} />
+                <div className="flex max-lg:flex-col lg:justify-between relative z-30 max-lg:justify-center md:pt-10 2xl:pb-[200px] md:pb-[140px] pb-20 max-w-[1408px] mx-auto  max-sm:pb-14">
+                    <Image className='xl:max-w-[750px] max-w-[540px] max-lg:mx-auto w-full xl:h-[534px]' src="/assets/images/webp/attivita-image.webp" alt="attivita" width={800.6} height={534} />
                     <div className='xl:pt-[92px] md:pt-10 pt-4'>
                         <h3 className='lg:max-w-[384px] max-w-none text-32 text-white font-black xl:pb-10 pb-6 max-sm:text-center leading-130 italic'>
                             Più tempo per gestire, meno tempo perso
                         </h3>
                         <div>
                             {VANTAGGI_LIST.map((vantaggio, index) => (
-                                <div key={index} className='flex gap-2 pb-4'>
+                                <div key={index} className='flex gap-2 pb-4 items-center'>
                                     <Image className='size-5' src="/assets/images/png/yellow-tick.png" alt='yellow-tick' width={20} height={19} />
                                     <p className='font-medium text-base text-white max-w-[506px]'>{vantaggio}</p>
                                 </div>
