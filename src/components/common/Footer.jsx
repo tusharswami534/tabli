32a9bd2824d9f6edf6d00335661889e9b1edfd33
Bimla-jain-555 <vj13798@gmail.com>
2025-04-23 15:58:47 +0530

    add link on footer logo

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
@@ -10,7 +11,9 @@ const Footer = () => {
       <div className='max-w-[1920px] mx-auto '> 
         <div className='bg-gray'>
           <div className="container mx-auto px-4 pt-20 pb-10">
-            <Image src="/assets/images/png/footer-logo.png" alt='footer-logo' width={123} height={33} className='w-[123px]  pb-6 flex mx-auto' />
+            <Link href="/" className='flex justify-center'>
+              <Image src="/assets/images/png/footer-logo.png" alt='footer-logo' width={123} height={33} className='w-[123px]  pb-6 flex mx-auto' />
+            </Link>
             <p className='text-base italic font-[900] text-white text-center'>Tutti i diritti riservati</p>
           </div>
         </div>
