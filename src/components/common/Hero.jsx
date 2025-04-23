12a63e16fa33a1dd97312b3c144e1cfaf4104562
TUSHAR SWAMI <tusharswami17@gamil.com>
2025-04-23 11:05:21 +0530

    add svh

--- src/components/common/Hero.jsx
+++ src/components/common/Hero.jsx
@@ -15,43 +15,43 @@ import Image from "next/image";
 const Hero = () => {
   const path = usePathname()
   return (
-    <div className={` pb-[72px] max-md:pb-0 relative px-4 overflow-hidden ${path === '/' ? 'bg-dark-blue' : '!bg-yellow'}`}>
+    <div className={` pb-[72px] max-md:pb-0 relative overflow-hidden ${path === '/' ? 'bg-dark-blue' : '!bg-yellow'}`}>
       <Link className='absolute max-md:hidden bottom-4 left-1/2 -translate-x-1/2' href={'#come-funziona'}>
         <Icons icon='downArrow' />
       </Link>
       <Nav />
       <div className='flex w-full py-[62px] max-md:pt-20 max-md:pb-0'>
-        <div className='container max-w-[1600px] mx-auto'>
-          <div className='flex items-center gap-5 max-lg:flex-col justify-between'>
-            <div className='flex flex-col max-w-[726px] w-full lg:w-6/12'>
-              <h1 style={{ textShadow: '0px 8px 16px 0px #00000029' }} className={`text-80 max-xl:text-7xl max-lg:text-6xl max-md:text-5xl max-sm:text-40 max-lg:text-center font-black italic leading-131 ${path === '/' ? 'text-white' : 'text-dark-grey'}`}>{path === '/' ? ' Tabli sta arrivando!' : 'Valorizza la tua attività con Tabli'}</h1>
+        <div className='container lg:px-4 max-w-[1632px] mx-auto'>
+          <div className='flex items-center gap-5 max-lg:gap-0 max-lg:flex-col justify-between'>
+            <div className='flex flex-col max-w-[726px] max-lg:px-4 w-full lg:w-6/12'>
+              <h1 className={`text-80 max-xl:text-7xl max-lg:text-6xl max-md:text-5xl max-sm:text-40 max-lg:text-center font-black italic leading-131 ${path === '/' ? 'text-white hero-text' : 'text-dark-grey'}`}>{path === '/' ? ' Tabli sta arrivando!' : 'Valorizza la tua attività con Tabli'}</h1>
               <Paragraph paragraphText={path === '/' ? 'Stiamo per rivoluzionare il modo in cui acquisti prodotti e servizi nei tuoi luoghi preferiti.' : 'Tabli è progettato per supportare ogni tipo di attività locale - dal bar all’hotel, dal lido alla piccola bottega - offrendo uno strumento semplice, versatile e pronto all’uso.'} className={`mb-2 max-lg:text-center ${path !== '/' && '!text-dark-grey'}`} />
               <Paragraph paragraphText={path === '/' ? 'Con Tabli, potrai consultare l’offerta, ordinare e pagare direttamente dal tuo smartphone, in totale autonomia e senza attese.' : 'Permetti ai tuoi clienti di consultare i tuoi prodotti e servizi, ordinare o prenotare in autonomia, migliorando l’esperienza d’acquisto senza rivoluzionare la tua routine lavorativa.'} className={`max-sm:hidden max-lg:text-center ${path !== '/' && '!text-dark-grey'}`} />
               <div className='flex gap-4 mt-10 max-sm:flex-col max-lg:items-center max-lg:justify-center'>
-                <CustomButton buttonName={'Come Funziona'} className={`max-w-[160px] leading-130 max-sm:max-w-[280px] w-full ${path !== '/' && '!text-white !bg-dark-blue'}`} />
-                <CustomButton buttonName={'Contatti'} className={`max-w-[160px] leading-130 border-transparent max-sm:max-w-[280px] w-full border border-solid !text-dark-blue !bg-white ${path === '/' ?   '!border-dark-blue' : '!border-transparent'}`} />
+                <CustomButton buttonName={'Come Funziona'} className={`max-w-[160px] leading-119 max-sm:max-w-[280px] w-full hover:!bg-white hover:!text-dark-blue ${path !== '/' && '!text-white !bg-dark-blue' }`} />
+                <CustomButton buttonName={'Contatti'} className={`max-w-[160px] leading-119 border-transparent max-sm:max-w-[280px] w-full border border-solid !text-dark-blue !bg-white ${path === '/' ?   '!border-dark-blue hover:!bg-yellow' : '!border-transparent hover:!bg-dark-blue hover:!text-white'}`} />
               </div>
             </div>
             <div className="w-full lg:w-6/12 max-lg:mt-20">
             <div className="max-w-[700px] max-lg:mx-auto ml-auto">
               <Swiper
                 slidesPerView={2.5}
-                spaceBetween={24}
+                spaceBetween={30}
                 centeredSlides={true}
                 loop={true}
                 breakpoints={{
                   439: {
                     slidesPerView: 3,
-                    spaceBetween: 16,
+                    spaceBetween: 25,
                   },
                 }}
-                className="mySwiper hero-slider py-10"
+                className="mySwiper hero-slider cursor-grab py-10"
               >
                 {HERO_SWIPER_DATA.map((obj, index) => {
                   return (
                     <SwiperSlide key={index}>
                       <div className="py-14 xl:py-[79px] flex justify-end">
-                        <Image
+                        <Image 
                           src={obj.image}
                           alt="hero-image"
                           width={197}
@@ -74,4 +74,3 @@ const Hero = () => {
 }
 
 export default Hero
-
--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -6,38 +6,38 @@ import { NavList } from '../../utils/helper'
 import { usePathname } from 'next/navigation'
 
 const Nav = () => {
-  const [open , setOpen] = useState(false)
+  const [open, setOpen] = useState(false)
   const path = usePathname()
 
   useEffect(() => {
     if (open) {
-      document.body.style.overflow = 'hidden'
+      document.body.classList.add('max-md:overflow-hidden')
     } else {
-      document.body.style.overflow = 'unset'
+      document.body.classList.remove('max-md:overflow-hidden')
     }
   }, [open])
 
   return (
-    <div className='w-full pt-10'>
-        <div className='container max-w-[1600px] mx-auto'>
-          <div className='w-full flex justify-between items-center'>
-            <Link href="/">
-              <Icons className={` transition-all duration-300 ${path !== '/' && 'fill-dark-grey'}`} icon='logo'/>
-            </Link>
-            <div className={`flex items-center max-md:fixed max-md:w-full max-md:h-full max-md:justify-center max-md:top-0  max-md:flex-col max-md:z-10 max-md:transition-all max-md:duration-300 gap-6 ${open ? 'left-0' : 'left-full'} ${path === '/' ? 'max-md:bg-dark-blue' : 'max-md:bg-yellow'}`}>
-                {NavList.map((item, index) => (
-                  <Link onClick={() => setOpen(false)} className={`font-black italic transition-all duration-300 max-md:text-xl leading-131 ${path === '/' ? 'text-white hover:text-yellow' : 'text-dark-grey hover:text-dark-blue'}`} href={item.link} key={index}>
-                    {item.title}
-                  </Link>
-                ))}
-            </div>
-           <button onClick={() => setOpen(!open)} className={`w-[46px] relative z-20 hidden max-md:flex h-[45px] cursor-pointer overflow-hidden border-2 border-solid gap-1 items-center justify-center flex-col rounded-lg ${path === '/' ? 'border-yellow' : 'border-dark-blue'}`}>
-                <span className={`w-6 h-0.5 flex rounded-full transition-all duration-300 ${open  && 'translate-x-20'} ${path === '/' ? 'bg-yellow' : 'bg-dark-blue'}`}></span>
-                <span className={`w-[18px] h-0.5 flex  transition-all duration-300 rounded-full relative after:absolute after:w-full after:h-full after:rounded-full  after:top-0 after:left-0 after:transition-all after:duration-300 ${open  && 'rotate-45 !w-8 after:rotate-90'} ${path === '/' ? 'bg-yellow after:bg-yellow' : 'bg-dark-blue after:bg-dark-blue'}`}></span>
-                <span className={`w-3.5 h-0.5 flex  transition-all duration-300 rounded-full ${open  && '-translate-x-20'} ${path === '/' ? 'bg-yellow' : 'bg-dark-blue'}`}></span>
-           </button>
+    <div className='w-full pt-10 px-4'>
+      <div className='container max-w-[1600px] mx-auto'>
+        <div className='w-full flex justify-between items-center'>
+          <Link href="/">
+            <Icons className={` transition-all duration-300 ${path !== '/' && 'fill-dark-grey'}`} icon='logo' />
+          </Link>
+          <div className={`flex items-center max-md:fixed max-md:w-full max-md:h-full max-md:justify-center max-md:top-0  max-md:flex-col max-md:z-10 max-md:transition-all max-md:duration-300 gap-6 ${open ? 'left-0' : 'left-full'} ${path === '/' ? 'max-md:bg-dark-blue' : 'max-md:bg-yellow'}`}>
+            {NavList.map((item, index) => (
+              <Link onClick={() => setOpen(false)} className={`font-black italic transition-all duration-300 max-md:text-xl leading-131 ${path === '/' ? 'text-white hover:text-yellow' : 'text-dark-grey hover:text-dark-blue'}`} href={item.link} key={index}>
+                {item.title}
+              </Link>
+            ))}
           </div>
+          <button onClick={() => setOpen(!open)} className={`w-[46px] relative z-20 hidden max-md:flex h-[45px] cursor-pointer overflow-hidden border-2 border-solid gap-1 items-center justify-center flex-col rounded-lg ${path === '/' ? 'border-yellow' : 'border-dark-blue'}`}>
+            <span className={`w-6 h-0.5 flex rounded-full transition-all duration-300 ${open && 'translate-x-20'} ${path === '/' ? 'bg-yellow' : 'bg-dark-blue'}`}></span>
+            <span className={`w-[18px] h-0.5 flex  transition-all duration-300 rounded-full relative after:absolute after:w-full after:h-full after:rounded-full  after:top-0 after:left-0 after:transition-all after:duration-300 ${open && 'rotate-45 !w-8 after:rotate-90'} ${path === '/' ? 'bg-yellow after:bg-yellow' : 'bg-dark-blue after:bg-dark-blue'}`}></span>
+            <span className={`w-3.5 h-0.5 flex  transition-all duration-300 rounded-full ${open && '-translate-x-20'} ${path === '/' ? 'bg-yellow' : 'bg-dark-blue'}`}></span>
+          </button>
         </div>
+      </div>
     </div>
   )
 }
