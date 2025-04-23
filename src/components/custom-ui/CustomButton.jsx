a230ab6150e9921e342b3c75f6a02aeb91bacd0e
TUSHAR SWAMI <tusharswami17@gamil.com>
2025-04-23 16:06:45 +0530

    add hover on button

--- src/components/custom-ui/CustomButton.jsx
+++ src/components/custom-ui/CustomButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CustomButton = ({buttonName , myClass , className}) => {
   return (
-    <button className={`px-4 pt-3.5 font-black uppercase italic shadow-common-button cursor-pointer transition-all duration-300 bg-yellow rounded-[40px] text-dark-grey pb-2.5 ${myClass , className}`}>
+    <button className={`px-4 pt-3.5 font-black uppercase italic shadow-common-button hover:bg-white hover:text-dark-blue cursor-pointer transition-all duration-300 bg-yellow rounded-[40px] text-dark-grey pb-2.5 ${myClass , className}`}>
       {buttonName}
     </button>
   )
