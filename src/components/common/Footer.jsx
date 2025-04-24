984a56ede89950535ed83fe365aaca6010f1deed
Bimla-jain-555 <vj13798@gmail.com>
2025-04-24 16:22:29 +0530

    change some updates

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div className='relative bg-gray'>
+    <div className='relative bg-gray -mb-2'>
       <div className='absolute 2xl:top-[-82%] xl:top-[-50%] md:top-[-30%] top-[-16%] left-0 w-full h-full pointer-events-none'>
         <Image src="/assets/images/png/footer-top-layer.png" alt="Footer Image" width={1920} height={1080} className="2xl:h-[200px] w-full max-xl:w-[1920px] pointer-events-none max-sm:object-cover"/>
       </div>
